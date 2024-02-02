b92deebf4a4cea0f61dcd395c125ef73ad84f6d1
DesignLedDublin <125903153+DesignLedDublin@users.noreply.github.com>
2024-02-02 11:18:53 +0000

    Create button on quiz page template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM fully loaded and parsed");
+});
 
+let finalScore = 0;
 
- 
 /** Function that reacts when the user chooses an answer */
 function chooseAnAnswer() {
-    let choices = document.querySelectorAll(".choices");
+  let choices = document.querySelectorAll(".choices");
 
-    choices.forEach(choice => {
-      choice.addEventListener("click", function() {
-        choices.forEach(otherChoice => {
-          otherChoice.classList.remove("chosen");
-        });
-        choice.classList.add("chosen");
+  choices.forEach((choice) => {
+    choice.addEventListener("click", function () {
+      choices.forEach((otherChoice) => {
+        otherChoice.classList.remove("chosen");
       });
+      choice.classList.add("chosen");
     });
+  });
+
 };
 
 chooseAnAnswer();
 
+/** Function that decides which is the correct answer */
+function decideIfCorrect() {
 
+    let correctAnswer = document.querySelectorAll(".correct-answer");
+    
+    if (chooseAnAnswer == correctAnswer) {
+        finalScore++;
+    } else {
+        finalScore;
+    };
+};
 
-// function decideIfCorrect() {
-
-
-// };
+decideIfCorrect();
 
 // function addToScore() {
 
