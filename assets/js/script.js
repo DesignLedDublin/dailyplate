8275391bb93cd0ee324fe9e597605611092054c7
DesignLedDublin <125903153+DesignLedDublin@users.noreply.github.com>
2024-02-25 15:18:01 +0000

    Remove notes from new function inserted as it was being constructed

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,16 +39,49 @@ function decideIfCorrectIncrementScore() {
     document.getElementById("wrong-or-right").innerHTML = "";}, 3000); 
 }
 
-/** Function that takes user to the next question and calls decideIfCorrect function */
-function nextQuestion() {
-  document.getElementById("quiz-next-question")
-    .addEventListener("click", function () {
-      decideIfCorrectIncrementScore();
 
-    });
+
+function nextQuestion(pagesMap) {
+  const handleNextClick = () => {
+    const currentPage = window.location.pathname.split('/').pop();
+
+    const nextPage = pagesMap[currentPage];
+  
+    if (nextPage) {
+      decideIfCorrectIncrementScore();
+  
+      document.getElementById("quiz-next-question").disabled = true;
+      setTimeout(() => {
+        document.getElementById("quiz-next-question").disabled = false;
+      }, 3000);
+  
+      setTimeout(() => {
+        window.location.href = nextPage;
+      }, 3000);
+    } else {
+      console.error('Next page not found in pagesMap');
+    }
+  
+    return false;
   };
 
-nextQuestion();
+  document.getElementById("quiz-next-question").addEventListener("click", handleNextClick);
+}
+
+const pagesMap = {
+  'quiz-page-1.html': 'quiz-page-2.html',
+  'quiz-page-2.html': 'quiz-page-3.html',
+  'quiz-page-3.html': 'quiz-page-4.html',
+  'quiz-page-4.html': 'quiz-page-5.html',
+  'quiz-page-5.html': 'quiz-page-6.html',
+  'quiz-page-6.html': 'quiz-page-final.html',
+  'quiz-page-final.html': 'score-page.html',
+};
+
+
+nextQuestion(pagesMap);
+
+
 
 /** Function that shows the user their score */
 function showScore() {
